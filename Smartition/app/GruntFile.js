b88226ecfcc862e0b1c419aca71e5d2764a8ede6
unknown <vlavella@uruit-43.carrasco.uruit.com>
2014-09-22 17:52:27 -0300

    agrego nuevo proyecto

--- Smartition/app/GruntFile.js
+++ Smartition/app/GruntFile.js
@@ -7,17 +7,24 @@ module.exports = function (grunt) {
         // get the configuration info from package.json ----------------------------
         pkg: grunt.file.readJSON('package.json'),
 
+        copy: {
+            main: {
+                files: [{ src: 'node_modules/bootstrap/fonts', dest: 'styles/fonts' },
+                        { src: 'node_modules/bootstrap/less', dest: 'styles/content/less' },
+                        { src: 'node_modules/bootstrap/dist/js/', dest: 'libs/' }]
+            }
+        },
         jshint: {
             options: {
                 reporter: require('jshint-stylish') // use jshint-stylish to make our errors look and read good
             },
 
-            // when this task is run, lint the Gruntfile and all js files in src
+            // when this task is run, lint the Gruntfile and all js files in the app
             build: ['Grunfile.js', 'app/**/*.js']
         },
-        uglify: {
+        uglify: {//when uglify run as a result we have all the application minified in the smartition.min.js file 
             options: {
-                banner: '\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>\n'
+                banner: '\n/* <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %>*/\n'
             },
             build: {
                 files: {
@@ -34,7 +41,7 @@ module.exports = function (grunt) {
 
     });
 
-    // LOAD GRUNT PLUGINS ========================================================
+    // LOAD GRUNT PLUGINS
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
